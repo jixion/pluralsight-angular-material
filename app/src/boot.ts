f992c5b2af996eacb2e819eb6e261c84764b0aa2
Jeff Burroughs <jburroughs@sparkimaginations.com>
2016-11-13 15:13:45 -0500

    bootsrapping the application

--- app/src/boot.ts
+++ app/src/boot.ts
@@ -1,2 +1,7 @@
 ///  <reference path="_all.ts" />
 
+module ContactManagerApp {
+    angular.module('contactManagerApp', ['ngMaterial'])
+        .controller('mainController', MainController)
+    ;
+}
